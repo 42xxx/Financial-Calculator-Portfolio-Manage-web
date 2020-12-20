1c95e081d61209f7efa170d7ed9893c2142dc544
YUWEN JIN <60263205+StarryYJ@users.noreply.github.com>
2020-12-19 23:36:44 -0500

    Update Runtime.py

--- Runtime.py
+++ Runtime.py
@@ -2,58 +2,68 @@ from time import perf_counter
 import pandas as pd
 from pandas import DataFrame
 import yfinance as yf
+from preloaded_data import preload
 import os
-runtime_yf = []
-runtime_csv = []
-os.getcwd()
-#runtimes using yfinance
 
-djia = ['MMM', 'AXP', 'AMGN', 'AAPL', 'BA', 'CAT', 'CVX', 'CSCO', 'DOW',
-         'GS', 'HON', 'IBM', 'INTC', 'JNJ', 'JPM', 'MCD', 'MRK', 'MSFT',
-         'NKE', 'PG', 'CRM', 'KO', 'HD', 'TRV', 'DIS', 'UNH', 'VZ', 'V',
-         'WBA', 'WMT']
+if __name__ == "main":
+	
+	preload()
+	runtime_yf, runtime_csv = [], []
+	os.getcwd()
+	
+	# runtimes using yfinance
 
-results = []
-for i in djia:
-    j = djia.index(i)
-    startTime = perf_counter()
-    yf.download(i, start='2019-12-16', end='2020-12-17')
-    endTime = perf_counter()
-    yf2 = (endTime - startTime)
-    if j > 1:
-        yf2 = yf2 + results[(j - 1)]
-    results.append(yf2)
+	djia = ['MMM', 'AXP', 'AMGN', 'AAPL', 'BA', 'CAT', 'CVX', 'CSCO', 'DOW',
+			 'GS', 'HON', 'IBM', 'INTC', 'JNJ', 'JPM', 'MCD', 'MRK', 'MSFT',
+			 'NKE', 'PG', 'CRM', 'KO', 'HD', 'TRV', 'DIS', 'UNH', 'VZ', 'V',
+			 'WBA', 'WMT']
+
+	
+	results = []
+	for i in djia:
+		j = djia.index(i)
+		startTime = perf_counter()
+		yf.download(i, start='2019-12-16', end='2020-12-17')
+		endTime = perf_counter()
+		yf2 = (endTime - startTime)
+		if j > 1:
+			yf2 = yf2 + results[(j - 1)]
+		results.append(yf2)
+
+	runtime_yf = DataFrame(results, columns=['runtime'])
+	runtime_yf.reset_index(inplace=True)
+	runtime_yf = runtime_yf.rename(columns = {'index':'number of stocks'})
+	runtime_yf['number of stocks'] += 1
 
 
-runtime_yf = DataFrame(results, columns=['runtime'])
-runtime_yf.reset_index(inplace=True)
-runtime_yf = runtime_yf.rename(columns = {'index':'number of stocks'})
-runtime_yf['number of stocks'] += 1
+	#runtimes using csv files
 
+	# For this project, we assume that the pre downloaded data is in a folder names 'data'
+	# under the same directory as the the python scripts.
 
-#runtimes using csv files
+	results = []
+	for i in djia:
+		j = djia.index(i)
+		startTime = perf_counter()
+		filename = "data/"+i + ".csv"
+		df = pd.read_csv(filename, encoding='utf-8')
+		endTime = perf_counter()
+		csv = (endTime - startTime)
+		if j > 1:
+			csv = csv + results[(j - 1)]
+		results.append(csv)
 
-#For this project, we assume that the data is in
-#the same directory as the .py file.
+	runtime_csv = DataFrame(results, columns=['runtime'])
+	runtime_csv.reset_index(inplace=True)
+	runtime_csv = runtime_csv.rename(columns = {'index':'number of stocks'})
+	runtime_csv['number of stocks'] += 1
 
-results = []
-for i in djia:
-    j = djia.index(i)
-    startTime = perf_counter()
-    filename = "data/"+i + ".csv"
-    df = pd.read_csv(filename, encoding='utf-8')
-    endTime = perf_counter()
-    csv = (endTime - startTime)
-    if j > 1:
-        csv = csv + results[(j - 1)]
-    results.append(csv)
-
-runtime_csv = DataFrame(results, columns=['runtime'])
-runtime_csv.reset_index(inplace=True)
-runtime_csv = runtime_csv.rename(columns = {'index':'number of stocks'})
-runtime_csv['number of stocks'] += 1
-
-runtimes = []
-runtimes = runtime_yf.join(runtime_csv, lsuffix='_yf', rsuffix='_csv')
-runtimes = runtimes.rename(columns ={'number of stocks_yf':'number of stocks'})
-runtimes.drop(columns=['number of stocks_csv'])
+	# clear up and print out
+	runtimes = []
+	runtimes = runtime_yf.join(runtime_csv, lsuffix='_yf', rsuffix='_csv')
+	runtimes = runtimes.rename(columns ={'number of stocks_yf':'number of stocks'})
+	runtimes = runtimes.drop(columns=['number of stocks_csv'])
+	
+	print(runtimes)
+	
+	
